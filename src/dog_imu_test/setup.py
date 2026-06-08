daa0e297558a9c51838c8b850e9754a1ac4a21b8
Vidura Munasinghe <81399281+vidurawarna@users.noreply.github.com>
2026-06-08 15:16:45 +0800

    Update setup.py

--- src/dog_imu_test/setup.py
+++ src/dog_imu_test/setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='maintainer1',
-    maintainer_email='todo@mail1',
+    maintainer_email='your.email@example.com',
     description='TODO: Package description',
     license='Apache-2.0',
     entry_points={
